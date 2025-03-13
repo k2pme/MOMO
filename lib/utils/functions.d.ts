87d7defe1b71c8752205ea04a110d407bcedc967
k2pme <116934755+k2pme@users.noreply.github.com>
2025-03-13 23:51:18 +0100

    Delete lib directory

--- lib/utils/functions.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const wichtig: {
-    bas64Encode: (apiUser: string, apiKey: string) => string;
-    bearerFormat: (b64token: string) => string;
-    basicFormat: (b64token: string) => string;
-    generateUUID: () => string;
-};
